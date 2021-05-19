7422b9eac6490bdb9ecc2a2a8185b633f8003cea
Daniel <danielgulic@gmail.com>
2021-05-19 10:12:46 +1000

    Remove button hover style when loading

--- src/frontend/src/Demo.tsx
+++ src/frontend/src/Demo.tsx
@@ -116,8 +116,8 @@ export default function Demo() {
 											className={`${
 												loading
 													? 'bg-indigo-400 cursor-not-allowed'
-													: 'bg-indigo-600'
-											} mx-2 w-full px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+													: 'bg-indigo-600 hover:bg-indigo-700'
+											} mx-2 w-full px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
 										>
 											{loading ? 'Verifying...' : 'Verify'}
 										</button>
